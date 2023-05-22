15171f34a3281e79989a0e18a632f96bd7385a37
Denis Krivosheev <denis.takeda@gmail.com>
2023-05-22 09:30:55 +0300

    Create a default configuration file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"log"
+
 	"github.com/denistakeda/mpass/internal/config"
 )
 
 func main() {
-	conf := config.ParseServerCfg()
+	conf, err := config.ParseServerCfg()
+	if err != nil {
+		log.Fatal("failed to read the configuration")
+	}
+
+	log.Printf("Configuration: %v\n", conf)
 }
