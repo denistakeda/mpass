2fbfac7a130f23c0b062d5c4b16fb5a4a25993c8
Denis Krivosheev <denis.takeda@gmail.com>
2023-06-16 15:05:06 +0300

    Insert record only if it's older than the latest one

--- internal/record_store/db.go
+++ internal/record_store/db.go
@@ -2,6 +2,7 @@ package record_store
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 
 	"github.com/denistakeda/mpass/internal/domain/record"
@@ -31,7 +32,7 @@ func (s *dbStore) AddRecords(ctx context.Context, login string, records []record
 		return nil
 	}
 
-	tx, err := s.db.BeginTx(ctx, nil)
+	tx, err := s.db.BeginTxx(ctx, nil)
 	if err != nil {
 		return fmt.Errorf("failed to start a transaction: %w", err)
 	}
@@ -39,28 +40,21 @@ func (s *dbStore) AddRecords(ctx context.Context, login string, records []record
 	for _, rec := range records {
 		switch r := rec.(type) {
 		case *record.LoginPasswordRecord:
-			tx.ExecContext(ctx,
-				"insert into login_password_record (id, last_update_date, login, password, user_login) values ($1, $2, $3, $4, $5)",
-				r.ID, r.LastUpdateDate, r.Login, r.Password, login,
-			)
+			err = upsertLoginPasswordRecord(ctx, tx, r, login)
 		case *record.BankCardRecord:
-			tx.ExecContext(ctx,
-				"insert into bank_card_record (id, last_update_date, card_number, month, day, code, user_login) values ($1, $2, $3, $4, $5, $6, $7)",
-				r.ID, r.LastUpdateDate, r.CardNumber, r.Month, r.Day, r.Code, login,
-			)
+			err = upsertBankCardRecord(ctx, tx, r, login)
 		case *record.BinaryRecord:
-			tx.ExecContext(ctx,
-				"insert into binary_record (id, last_update_date, binary, user_login) values ($1, $2, $3, $4)",
-				r.ID, r.LastUpdateDate, r.Binary, login,
-			)
+			err = upsertBinaryRecord(ctx, tx, r, login)
 		case *record.TextRecord:
-			tx.ExecContext(ctx,
-				"insert into binary_record (id, last_update_date, text, user_login) values ($1, $2, $3, $4)",
-				r.ID, r.LastUpdateDate, r.Text, login,
-			)
+			err = upsertTextRecord(ctx, tx, r, login)
 		default:
 			return fmt.Errorf("unknown record type: %v", r)
 		}
+	}
+
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to add records: %w", err)
 	}
 
 	if err := tx.Commit(); err != nil {
@@ -106,4 +100,80 @@ func (s *dbStore) getRecords(ctx context.Context, login string, tableName tableN
 			login,
 		)
 	}
+}
+
+func upsertLoginPasswordRecord(ctx context.Context, tx *sqlx.Tx, r *record.LoginPasswordRecord, userLogin string) error {
+	var old record.LoginPasswordRecord
+	err := tx.GetContext(ctx, &old, "select * from login_password_record where id=$1", r.ID)
+
+	if err != nil {
+		_, err = tx.ExecContext(ctx,
+			"insert into login_password_record (id, last_update_date, login, password, user_login) values ($1, $2, $3, $4, $5)",
+			r.ID, r.LastUpdateDate, r.Login, r.Password, userLogin,
+		)
+	} else if r.LastUpdateDate.After(old.LastUpdateDate) {
+		_, err = tx.ExecContext(ctx,
+			"update login_password_record set login=$1, password=$2",
+			r.Login, r.Password,
+		)
+	}
+
+	return err
+}
+
+func upsertBankCardRecord(ctx context.Context, tx *sqlx.Tx, r *record.BankCardRecord, userLogin string) error {
+	var old record.BankCardRecord
+	err := tx.GetContext(ctx, &old, "select * from bank_card_record where id=$1", r.ID)
+
+	if err != nil {
+		_, err = tx.ExecContext(ctx,
+			"insert into bank_card_record (id, last_update_date, card_number, month, day, code, user_login) values ($1, $2, $3, $4, $5, $6, $7)",
+			r.ID, r.LastUpdateDate, r.CardNumber, r.Month, r.Day, r.Code, userLogin,
+		)
+	} else if r.LastUpdateDate.After(old.LastUpdateDate) {
+		_, err = tx.ExecContext(ctx,
+			"update bank_card_record set card_number=$1, month=$2, day=$3, code=$4",
+			r.CardNumber, r.Month, r.Day, r.Code,
+		)
+	}
+
+	return err
+}
+
+func upsertBinaryRecord(ctx context.Context, tx *sqlx.Tx, r *record.BinaryRecord, userLogin string) error {
+	var old record.BinaryRecord
+	err := tx.GetContext(ctx, &old, "select * from binary_record where id=$1", r.ID)
+
+	if err != nil {
+		_, err = tx.ExecContext(ctx,
+			"insert into binary_record (id, last_update_date, binary, user_login) values ($1, $2, $3, $4)",
+			r.ID, r.LastUpdateDate, r.Binary, userLogin,
+		)
+	} else if r.LastUpdateDate.After(old.LastUpdateDate) {
+		_, err = tx.ExecContext(ctx,
+			"update binary_record set binary=$1",
+			r.Binary,
+		)
+	}
+
+	return err
+}
+
+func upsertTextRecord(ctx context.Context, tx *sqlx.Tx, r *record.TextRecord, userLogin string) error {
+	var old record.TextRecord
+	err := tx.GetContext(ctx, &old, "select * from text_record where id=$1", r.ID)
+
+	if err != nil {
+		_, err = tx.ExecContext(ctx,
+			"insert into text_record (id, last_update_date, text, user_login) values ($1, $2, $3, $4)",
+			r.ID, r.LastUpdateDate, r.Text, userLogin,
+		)
+	} else if r.LastUpdateDate.After(old.LastUpdateDate) {
+		_, err = tx.ExecContext(ctx,
+			"update text_record set text=$1",
+			r.Text,
+		)
+	}
+
+	return err
 }
